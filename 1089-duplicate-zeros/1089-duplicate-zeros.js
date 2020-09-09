3eee29de93b4ae481aa8d1ddcfb53841211fd196
Elena Fedosikhina <fedosikhina@gmail.com>
2020-09-09 19:32:06 +0200

    1089-1089-duplicate-zeros

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+const duplicateZeros = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === 0) {
+          arr.splice((i+1), 0, '0')
+          arr.length--
+      }
+  }
+};
